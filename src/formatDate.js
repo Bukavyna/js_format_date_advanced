c4f753d47c395d62a7bd5dbd4bf2469d5161e24a
Taras <luchakt2@gmail.com>
2025-05-11 16:04:37 +0300

    Changed the verification logic

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,34 +8,29 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const newFromFormat = fromFormat[fromFormat.length - 1];
-  const newToFormat = toFormat[toFormat.length - 1];
+  const fromStep = fromFormat[fromFormat.length - 1];
+  const toStep = toFormat[toFormat.length - 1];
 
-  const dateParts = date.split(newFromFormat);
-  const formatLabels = fromFormat.slice(0, -1);
-  const result = {};
+  const fromKeys = fromFormat.slice(0, -1);
+  const toKeys = toFormat.slice(0, -1);
 
-  for (let i = 0; i < formatLabels.length; i++) {
-    result[formatLabels[i]] = dateParts[i];
+  const parts = date.split(fromStep);
+  const map = {};
+
+  for (let i = 0; i < fromKeys.length; i++) {
+    map[fromKeys[i]] = parts[i];
   }
 
-  const formatTo = toFormat.slice(0, -1);
+  if (map['YYYY'] && !map['YY']) {
+    map['YY'] = map['YYYY'].slice(-2);
+  }
+  if (map['YY'] && !map['YYYY']) {
+    const yy = parseInt(map['YY'], 10);
+    map['YYYY'] = yy < 30 ? '20' + map['YY'] : '19' + map['YY'];
+  }
 
-  const newDateParts = formatTo.map((toKey) => {
-    if (toKey === 'YY' && result['YYYY']) {
-      return result['YYYY'].slice(-2);
-    }
-
-    if (toKey === 'YYYY' && result['YY']) {
-      const yy = parseInt(result['YY'], 10);
-
-      return yy < 30 ? '20' + result['YY'] : '19' + result['YY'];
-    }
-
-    return result[toKey];
-  });
-
-  return newDateParts.join(newToFormat);
+  const result = toKeys.map((key) => map[key]);
+  return result.join(toStep);
 }
 
 module.exports = formatDate;
